8de35ae4b0fb8c17d125bffe0ba2c98714134dd8
D1ogooo <diiogocosta11@gmail.com>
2024-05-20 06:50:17 -0300

    feito

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,35 +1,34 @@
 const sqliteConnection = require("../sqlite/index");
 
-class usersController {
+class UsersController {
   async create(req, res) {
-   const { name, email } = req.body;
+    const { name, email } = req.body;
 
-   try {
-    const database = await sqliteConnection()
-    const checkUserExists = await database.get(`SELECT * FROM users WHERE email = ?`, [email]);
+    try {
+      const database = await sqliteConnection();
+      for (let i = 0; i < 10000; i++) {
+        const uniqueName = `${name}_${i}`;
+        const uniqueEmail = `${i}_${email}`;
 
-    if (checkUserExists) {
-     console.error('Este email já está em uso.');
-     return res.status(404).json("Este email já está em uso.")
+        await database.run("INSERT INTO users (name, email) VALUES (?, ?)", [uniqueName, uniqueEmail]);
+      }
+
+      return res.status(201).json({ "message": "10.000 usuários criados com sucesso" });
+    } catch (error) {
+      console.error('Erro ao criar usuários:', error);
+      return res.status(500).json({ error: 'Erro interno do servidor.' });
     }
-
-    await database.run("INSERT INTO users (name, email) VALUES (?, ?)", [name, email]);
-    return res.status(201).json({ "Usuario criado": "sucess" });
-   } catch (error) {
-    console.error('Erro ao criar usuário:', error);
-    return res.status(500).json({ error: 'Erro interno do servidor.' });
-   }
   }
 
   async show(req, res) {
-    const database = await sqliteConnection()
     try {
-     const users = await database.all(`SELECT * FROM users`) 
-     res.status(200).json(users)
+      const database = await sqliteConnection();
+      const users = await database.all(`SELECT * FROM users`);
+      res.status(200).json(users);
     } catch (error) {
-     res.status(404).json(error)
+      res.status(404).json(error);
     }
   }
 }
 
-module.exports = usersController;
+module.exports = UsersController;
